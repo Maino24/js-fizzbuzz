6ef379533235262a4bc9e8975da3c0860fc99418
Maino24 <mg02.gabriele@gmail.com>
2022-10-21 15:45:47 +0200

    stampati in console numeri e stringhe giuste

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,11 @@
-
+/*
 
 i = ``
 
-for (let i = 1; i < 101; i++){
 
 
-
+for ( i = 1; i < 101; i++){
+    console.log(i)
     if(i % 3 === 0){
         i = `Fizz`
         console.log(i)
@@ -20,12 +20,38 @@ for (let i = 1; i < 101; i++){
         console.log(i)
     }
 
+
+
+}*/
+
+
+
+
+
+
+i=``
+
+
+for (let i = 1; i<=100; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+ 
+     console.log ('FizzBuzz')
     
-    
-}
 
+    }
+ 
+    else if (i % 3 === 0){
+     console.log ('Fizz')
+    }
+ 
+    else if (i % 5 === 0 ) {
+     console.log ('Buzz')
+    }
+ 
+    else {
+     console.log (i)
+    }
+ }
 
-console.log(i)
-
-
-document.getElementById(`number`).innerHTML = `${i}`
+ document.getElementById(`list`).innerHTML = `${i}`

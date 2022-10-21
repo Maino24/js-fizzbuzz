e86ce3c20e3b2c7f6912dedf8e695bd8e1f12678
Maino24 <mg02.gabriele@gmail.com>
2022-10-21 17:42:07 +0200

    .

--- javascript/main.js
+++ javascript/main.js
@@ -28,30 +28,39 @@ for ( i = 1; i < 101; i++){
 
 
 
-
-i=``
+let divNumero = document.getElementById(`number`);
 
 
 for (let i = 1; i<=100; i++) {
-
+    
     if (i % 3 === 0 && i % 5 === 0) {
  
-     console.log ('FizzBuzz')
-    
+     console.log (`FizzBuzz`)
+     
+     let stringa = `${i}`;
+    divNumero.innerHTML += stringa;
 
     }
  
     else if (i % 3 === 0){
      console.log ('Fizz')
+     
+     let stringa = `${i}`;
+    divNumero.innerHTML += stringa;
     }
  
     else if (i % 5 === 0 ) {
      console.log ('Buzz')
+     
+     let stringa = `${i}`;
+    divNumero.innerHTML += stringa;
     }
  
     else {
      console.log (i)
+     let stringa = `${i}`;
+    divNumero.innerHTML += stringa;
     }
+
  }
 
- document.getElementById(`list`).innerHTML = `${i}`

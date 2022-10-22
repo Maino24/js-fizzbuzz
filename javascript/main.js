45ae7957ec99a42d4884f7a38537ea3be4d978ae
Maino24 <mg02.gabriele@gmail.com>
2022-10-22 11:36:08 +0200

    create condizioni e stampate in console

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,24 @@
 let numberOne = document.getElementById(`number`);
+let cont = document.getElementById(`container`);
+
 
 /* ciclo for */
 
 for(let i = 1; i < 101; i++){
 
     console.log(i)
+
     /* scrivo le condizioni */
-    
+
+    if((i % 3 === 0) && (i % 5 === 0)){
+        console.log(`FizzBuzz`)
+    }else if(i % 3 === 0){
+        console.log(`Fizz`)
+    }else if(i % 5 === 0){
+        console.log(`Buzz`)
+    }else{
+        console.log(i)
+    }
 
 
 

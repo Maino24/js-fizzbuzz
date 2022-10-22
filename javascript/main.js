0d9e80d1c59928f44c07b5f0f764f0688d6125d9
Maino24 <mg02.gabriele@gmail.com>
2022-10-22 12:02:04 +0200

    stampata lista in html

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,33 @@
-let numberOne = document.getElementById(`number`);
-let cont = document.getElementById(`container`);
-
+let container = document.getElementById(`container`)
 
 /* ciclo for */
-
 for(let i = 1; i < 101; i++){
-
+    let divOne = document.createElement(`div`)
+    divOne.classList.add(`box-js`)
     console.log(i)
 
     /* scrivo le condizioni */
 
     if((i % 3 === 0) && (i % 5 === 0)){
+        divOne.innerText = `FizzBuzz`
+        divOne.classList.add(`blue`)
         console.log(`FizzBuzz`)
     }else if(i % 3 === 0){
+        divOne.innerText = `Fizz`
+        divOne.classList.add(`red`)
         console.log(`Fizz`)
     }else if(i % 5 === 0){
+        divOne.innerText = `Buzz`
+        divOne.classList.add(`red`)
         console.log(`Buzz`)
     }else{
+        divOne.innerText = `${i}`
+        divOne.classList.add(`red`)
         console.log(i)
     }
 
 
-
+    container.append(divOne)
 
 }
+
